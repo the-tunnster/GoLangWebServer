d283fc636272a270a9cc0d20c6196ea8dd2fecf0
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-02 16:07:43 +0530

    Separated the functions from main into their own space.

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "This is the home page.")
-}
-
-func About(w http.ResponseWriter, r *http.Request){
-
-	sum := AddValues(6, 9)
-	fmt.Fprintf(w, "This is the about page.\n")
-	fmt.Fprintf(w, "The sum of two values is %d.", sum)
-
-}
-
-func AddValues(x, y int) int {	return(x+y)		}
 
 func main(){
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
-	fmt.Println("Starting application on port", portNumber)
+	log.Println("Starting application on port", portNumber)
 
 	http.ListenAndServe(portNumber, nil)
 
