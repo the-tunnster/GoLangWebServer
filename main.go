643f55276752df89454d18254f86d1395502b9eb
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-02 12:07:37 +0530

    Added some lines of code, and two more handlers.

--- main.go
+++ main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the home page.")
+}
+
+func About(w http.ResponseWriter, r *http.Request){
+
+	sum := AddValues(6, 9)
+	fmt.Fprintf(w, "This is the about page.\n")
+	fmt.Fprintf(w, "The sum of two values is %d.", sum)
+
+}
+
+func AddValues(x, y int) int {	return(x+y)		}
+
 func main(){
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello World.")
-		if(err!=nil){
-			log.Println("Error while writing : ", err)
-		}else{
-			log.Println("Number of bytes written : ", n)
-		}
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Starting application on port", portNumber)
+
+	http.ListenAndServe(portNumber, nil)
 
 }
