3f55e608a4a8b84641d4b9bea6a6bc7877b41dc6
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-02 17:57:00 +0530

    Added a whole bunch of stuff for this piece of code. Will add documentation for each function at a later date.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,15 +3,62 @@ package render
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
+var functions = template.FuncMap{
+
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string){
 
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	
+	_, err := RenderTemplateTest(w)
 	if(err!=nil){
-		fmt.Println("Error parsing template :", err)
+		fmt.Println("Error getting template cahce : ", err)
+	}
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err = parsedTemplate.Execute(w, nil)
+	if(err!=nil){
+		fmt.Println("Error parsing template : ", err)
 	}
 	
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error){
+
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if(err!=nil){
+		return myCache, err
+	}
+
+	for _, page := range pages{
+
+		name := filepath.Base(page)
+		fmt.Println("Page is currently : ", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if(err!=nil){
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if(err!=nil){
+			return myCache, err
+		}
+
+		if(len(matches)>0){
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if(err!=nil){
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+
 }
