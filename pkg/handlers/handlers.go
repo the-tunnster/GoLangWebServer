5f5af012910ae131a7d0c0bbcc4a0f355275a24f
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-06 11:04:18 +0530

    Implemented a basic session use case for testing.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -2,8 +2,8 @@ package handlers
 
 import (
 	"WebServer/pkg/config"
-	"WebServer/pkg/render"
 	"WebServer/pkg/models"
+	"WebServer/pkg/render"
 	"net/http"
 )
 
@@ -27,13 +27,22 @@ func NewHandlers(r *Repository){
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request){
+
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request){
 
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
