60bc1cce040b2687069db79e305d3990a8cb16c1
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-15 11:27:24 +0530

    Added CSRF tokens.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{
@@ -22,11 +24,12 @@ func NewTemplates(a *config.AppConfig){
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData{
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData{
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData){
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData){
 
 	var tc map[string]*template.Template
 
@@ -43,7 +46,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
