de9579d9b512869f8d431dcba49c89b1c751eb09
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-15 11:26:57 +0530

    Created a handler function and type for JSON Responses.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,52 +4,77 @@ import (
 	"WebServer/pkg/config"
 	"WebServer/pkg/models"
 	"WebServer/pkg/render"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 //Is the the repository type
-type Repository struct{
+type Repository struct {
 	App *config.AppConfig
+}
+
+type jsonResponse struct {
+	OK      bool `json:"ok"`
+	Message string `json:"message"`
 }
 
 var Repo *Repository
 
 //Creates a new repository
-func NewRepo(a *config.AppConfig) *Repository{
+func NewRepo(a *config.AppConfig) *Repository {
 	return &Repository{
-		App : a,
+		App: a,
 	}
 }
 
 //Sets the repository for handlers
-func NewHandlers(r *Repository){
+func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-func (m *Repository) Home(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
 
-func (m *Repository) About(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{})
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s.", start, end)))
+}
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK: true,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "    ")
+	if(err!=nil){
+		log.Println(err)
+	}
+
+	log.Println(string(out))
+	w.Header().Set("Contect-Type", "application/json")
+	w.Write(out)
 }
 
-func (m *Repository) Contact(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
 }
-
-func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
-}
-
-func (m *Repository) Availability(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
-}
-
-func (m *Repository) Generals(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
-}
-
-func (m *Repository) Majors(w http.ResponseWriter, r *http.Request){
-	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
 }
