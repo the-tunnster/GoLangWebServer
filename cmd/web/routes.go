d1d2c102bdc66897b078de382d0931d505a3cbbd
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-15 11:26:29 +0530

    Added new route for JSON responder.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,6 +23,9 @@ func routes(app *config.AppConfig) http.Handler{
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+
 
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
