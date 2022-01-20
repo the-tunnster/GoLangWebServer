3a76da3ded29a3a0bece6a2ac3420100e1a99ec7
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-20 13:42:26 +0530

    Changed the indentation of the struct.

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,12 +1,16 @@
 package models
+
+import "WebServer/internal/forms"
+
 //Holds data sent from handlers to templates to be integrated.
-type TemplateData struct{
-	StringMap 	map[string]string
-	IntMap 	map[string]int
-	FloatMap 	map[string]float32
-	Data 		map[string]interface{}
-	CSRFToken	string
-	Flash		string
-	Warning	string
-	Error 	string
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
+	Form      *forms.Form
 }
