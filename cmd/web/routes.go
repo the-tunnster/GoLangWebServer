d58ed12801dcfd86077f01bbc7ed3cbc83a88301
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-24 12:24:38 +0530

    Added some spacing.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,10 +23,11 @@ func routes(app *config.AppConfig) http.Handler{
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
+
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
-
 
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
