f4c15dfc45d60165a33ce89dead3581652bc2a0d
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-24 12:25:17 +0530

    Added form validating handlers.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -17,7 +17,7 @@ type Repository struct {
 }
 
 type jsonResponse struct {
-	OK      bool `json:"ok"`
+	OK      bool   `json:"ok"`
 	Message string `json:"message"`
 }
 
@@ -35,8 +35,6 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-
-
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
@@ -49,19 +47,52 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
 
-
-
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+
+	var emptyReservation models.Reservation
+
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Phone:     r.Form.Get("phone"),
+		Email:     r.Form.Get("email"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+
+		return
+	}
+
 }
-
-
 
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
@@ -74,24 +105,20 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s.", start, end)))
 }
 
-
-
 func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	resp := jsonResponse{
-		OK: true,
+		OK:      true,
 		Message: "Available!",
 	}
 
 	out, err := json.MarshalIndent(resp, "", "    ")
-	if(err!=nil){
+	if err != nil {
 		log.Println(err)
 	}
 
 	w.Header().Set("Contect-Type", "application/json")
 	w.Write(out)
 }
-
-
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
