40d587980f29e76e669e01bd2a86dbfe5ffe7c3c
the-tunnster <therealtarunkrishnan@gmail.com>
2022-01-24 12:24:53 +0530

    Added some validation.

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 //Custom form struct and embeds a url.values object
@@ -19,9 +21,35 @@ func New(data url.Values) *Form {
 	}
 }
 
+//Checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank.")
+		}
+	}
+}
+
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
-	if(x==""){
+	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank.")
+		return false
+	}
+	return true
+}
+
+//Valid returns true if there are no errors in the form
+func (f *Form) Valid() bool {
+	return (len(f.Errors) == 0)
+}
+
+//Checks for minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if(len(x)<length){
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long.", length))
 		return false
 	}
 	return true
